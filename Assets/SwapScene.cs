49e3b7b429dd646c5fa8f22ad9f6b7238066930d
000Shenzu000 <tomi5.rissanen@gmail.com>
2024-02-21 15:46:48 +0200

    StartMenu
    Start UI and bgm

---
+++ Assets/SwapScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScene : MonoBehaviour
+{
+    public void LoadAiti()
+    {
+        SceneManager.LoadScene("Minun Aitini");
+    }
+    public void LoadIsa()
+    {
+        SceneManager.LoadScene("Minun Isani");
+    }
+}
