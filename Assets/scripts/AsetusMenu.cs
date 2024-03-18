6cbca7f5ec178bf868902adf7ba92bc59c0d150d
rajalaeelis <eelisrajala05@gmail.com>
2024-03-19 00:29:28 +0200

    asetusmenun tekoa

--- Assets/scripts/AsetusMenu.cs
+++ Assets/scripts/AsetusMenu.cs
@@ -5,10 +5,49 @@ using UnityEngine.Audio;
 
 public class AsetusMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject asetusMenuUI;
+   
    public AudioMixer audioMixer;
 
-   public void SetVolume (float volume){
+    public FirstPersonLook Cameramovement;
+    public GameObject playerCam;
+
+    public void SetVolume (float volume){
 
         audioMixer.SetFloat("volume", volume);
    } 
+    public void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            
+                Pause();
+           
+        }
+    }
+
+    public void Resume ()
+    {
+       asetusMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cameramovement.enabled = (true);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        asetusMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cameramovement.enabled = (false);
+    }
+
+    public void PoistuPelista()
+    {
+        Debug.Log("poistupelistä");
+    }
+
 }
