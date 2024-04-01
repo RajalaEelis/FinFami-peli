38e4a06c0a2a86668c1a0bf0562c54e225eb6f60
000Shenzu000 <tomi5.rissanen@gmail.com>
2024-04-01 15:08:33 +0300

    Ching Chong
    juuh ei täs jaksa ennää kirjotella tehtiin hömmeleitä sinne ja tarina kulkee jep jep

---
+++ Assets/KenkaSCRIPT.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KenkaSCRIPT : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TP;
+    public GameObject MSG;
+    public FirstPersonMovement Controller;
+    public FirstPersonLook Cameramovement;
+    public Rigidbody m_Rigidbody;
+    void OnTriggerEnter()
+    {
+        MSG.SetActive(true);
+        Controller.enabled = (false);
+        Cameramovement.enabled = (false);
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Teleport()
+    {
+        Player.transform.position = TP.transform.position;
+
+        m_Rigidbody.constraints = RigidbodyConstraints.None;
+
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+    }
+}
